d304bfb11c2f9ae2a9ac95df86854b9a968abdf4
Edward Vetter-Drake <edward.vetterdrake@alteryx.com>
2021-06-07 12:53:01 -0600

    updating test names

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,6 @@
 fixture('test').page('http://localhost:3000/');
 
-test.skip('test', async t => {
+test.skip('clicking on a link', async t => {
 	await t.maximizeWindow()
 	await t.wait(5000);
 
@@ -13,7 +13,7 @@ test.skip('test', async t => {
 })
 
 
-test('test 2', async t => {
+test('clicking on a node', async t => {
 	await t.maximizeWindow()
 	await t.wait(5000);
 
