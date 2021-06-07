159f3338937c0a7877299f093981d203c31e8d8a
Edward Vetter-Drake <edward.vetterdrake@alteryx.com>
2021-06-07 13:17:14 -0600

    adding cypress example

---
+++ cypress/integration/examples/test.spec.js
@@ -0,0 +1,11 @@
+describe('clicking a node', () => {
+	it('can be clicked', async () => {
+
+		await cy.visit('http://localhost:3000/');
+
+		await cy.wait(5000);
+
+		await cy.get('canvas').dblclick(285, 72);
+
+	})
+})
